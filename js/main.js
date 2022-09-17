12bea97be390572bb890ee9e62628377c3c09fd6
Enrique Saracho Felix <esarachof97@gmail.com>
2022-09-17 12:52:26 -0600

    Update index.html js/main.js
    Updated files

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import ToDoItem from "./todoitem.js";
 
 const toDoList = new ToDoList();
 
-//Launch app
+// Launch app
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "complete") {
     initApp();
